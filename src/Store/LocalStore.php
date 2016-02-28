2643641d256988d25a33c41acdc2f1c41764909b
Marc Wörlein <marc.woerlein@perbility.de>
2016-02-28 12:42:12 +0100

    HX-9491: fix locking and code cleanup

--- src/Store/LocalStore.php
+++ src/Store/LocalStore.php
@@ -7,15 +7,31 @@ use DateTime;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Yaml\Yaml;
 
+/**
+ * Local store to handle locking and persist application data
+ *
+ * @package Perbility\Cilex\Store
+ */
 class LocalStore
 {
     const DATA_DIR = 'data';
     const LOCK_FILE = 'lock';
     
+    /** @var string */
     private $path;
-    private $locked = false;
-    private $log;
     
+    /** @var resource */
+    private $lockHandle;
+    
+    /** @var LoggerInterface */
+    private $log;
+
+    /**
+     * @param string $path
+     * @param LoggerInterface $log
+     *
+     * @throws Exception
+     */
     public function __construct($path, LoggerInterface $log)
     {
         if (!is_writable($path)) {
@@ -26,13 +42,18 @@ class LocalStore
         $this->log = $log;
     }
     
+    /**
+     */
     public function __destruct()
     {
-        if ($this->locked) {
+        if (null !== $this->lockHandle) {
             $this->unlock();
         }
     }
     
+    /**
+     * removes all data from store
+     */
     public function clear()
     {
         $it = new DirectoryIterator($this->path);
@@ -49,26 +70,37 @@ class LocalStore
         $this->log->info("Cleared local-store at " . $this->path);
     }
     
+    /**
+     * initialize empty store
+     */
     public function init()
     {
         foreach ($this->getDirectoryNames() as $dir) {
             mkdir($this->path . '/' . $dir);
         }
+        touch($this->path . '/' . self::LOCK_FILE);
         
         $this->setValue('initialized', [
             'timestamp' => (new DateTime())->format('c'),
             'username'  => trim(shell_exec('whoami'))
         ]);
-        
-        touch($this->path . '/' . self::LOCK_FILE);
         $this->log->info("Initialized local-store at " . $this->path);
     }
     
+    /**
+     * @return bool
+     */
     public function isInitialized()
     {
         return null !== $this->getValue('initialized');
     }
     
+    /**
+     * @param string $key
+     * @param mixed $value
+     *
+     * @throws Exception if store is not initialized
+     */
     public function set($key, $value)
     {
         if (!$this->isInitialized()) {
@@ -77,59 +109,86 @@ class LocalStore
         $this->setValue($key, $value);
     }
     
+    /**
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     * 
+     * @throws Exception if store is not initialized
+     */
     public function get($key, $default = null)
     {
         if (!$this->isInitialized()) {
             throw new Exception("Local store is not initialized");
         }
-        $this->getValue($key, $default);
+        return $this->getValue($key, $default);
     }
-    
-    public function lock()
+
+    /**
+     * Locks the store exclusive
+     * @param bool $nonblocking
+     *
+     * @throws Exception if store is not initialized
+     * @throws Exception if lock cannot be acquired
+     */
+    public function lock($nonblocking = false)
     {
         if (!$this->isInitialized()) {
             throw new Exception("Local store is not initialized");
         }
         
-        if ($this->locked) {
+        if (null !== $this->lockHandle) {
             return;
         }
         
-        $handle = fopen($this->path . '/' . self::LOCK_FILE, 'r');
-        if (!flock($handle, LOCK_EX)) {
+        $handle = fopen($this->path . '/' . self::LOCK_FILE, 'r+');
+        if (!flock($handle, $nonblocking ? LOCK_EX|LOCK_NB : LOCK_EX)) {
             throw new Exception("Lock failed");
         }
         
-        $this->locked = true;
-        fclose($handle);
+        $this->lockHandle = $handle;
         $this->log->debug("Acquired lock");
     }
-    
+
+    /**
+     * Release exclusiv lock
+     * 
+     * @throws Exception if store is not initialized
+     * @throws Exception if lock cannot be released
+     */
     public function unlock()
     {
         if (!$this->isInitialized()) {
             throw new Exception("Local store is not initialized");
         }
         
-        if (!$this->locked) {
+        if (null === $this->lockHandle) {
             return;
         }
         
-        $handle = fopen($this->path . '/' . self::LOCK_FILE, 'r');
-        if (!flock($handle, LOCK_UN)) {
+        if (!flock($this->lockHandle, LOCK_UN)) {
             throw new Exception("Unlock failed");
         }
         
-        $this->locked = false;
-        fclose($handle);
+        fclose($this->lockHandle);
+        $this->lockHandle = null;
         $this->log->debug("Released lock");
     }
     
+    /**
+     * all known subdirectories of the store, can be overwritten from spezialized store implementations
+     * 
+     * @return string[]
+     */
     protected function getDirectoryNames()
     {
         return [static::DATA_DIR];
     }
     
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
     private function setValue($key, $value)
     {
         $value = Yaml::dump($value);
@@ -142,6 +201,11 @@ class LocalStore
         file_put_contents($file, $value);
     }
     
+    /**
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
     private function getValue($key, $default = null)
     {
         $file = sprintf(
