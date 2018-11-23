cc205c16dc73ff259204b67f3a6f40c174828555
Sven Huessner <sven.huessner@perbility.de>
2018-11-23 11:21:51 +0100

    SYSOP-700 first working draft before refactor to TargetAwareMsTeamsHandler

--- src/Provider/MsTeamsMonologServiceProvider.php
+++ src/Provider/MsTeamsMonologServiceProvider.php
@@ -3,6 +3,9 @@ namespace Perbility\Cilex\Provider;
 
 use Cilex\Application;
 use Cilex\ServiceProviderInterface;
+use CMDISP\MonologMicrosoftTeams\TeamsLogHandler;
+use Monolog\Logger;
+use Psr\Log\NullLogger;
 
 /**
  * A provider that adds logging handlers to MS-Teams to a logger.
@@ -19,5 +22,44 @@ class MsTeamsMonologServiceProvider implements ServiceProviderInterface
      */
     public function register(Application $app)
     {
+        // Define a logger to be added to $app and store it as a service
+        $app['msteams'] = $app->share(
+            function () use ($app) {
+                if (empty($app['msteams.webhooks'])) {
+                    return new NullLogger();
+                }
+                
+                $log = new Logger('msteams');
+                $app['msteams.configure']($log);
+                return $log;
+            }
+        );
+        
+        // Define handlers for the above created logger and store them as configurations
+        $app['msteams.configure'] = $app->protect(
+            function (Logger $logger) use ($app) {
+                $webhookConfigs = $app['msteams.webhooks'];
+                
+                // Handler defaults
+                $defaults = [
+                    'name' => 'MS Teams',
+                    'level' => Logger::INFO,
+                    'bubble' => true,
+                ];
+                
+                // Create handlers for the logger according to config
+                foreach ($webhookConfigs as $webhookConfig) {
+                    $webhookConfig += $defaults;
+                    
+                    $logger->pushHandler(
+                        new TeamsLogHandler(
+                            $webhookConfig['url'],
+                            Logger::toMonologLevel($webhookConfig['level']),
+                            $webhookConfig['bubble']
+                        )
+                    );
+                }
+            }
+        );
     }
 }
